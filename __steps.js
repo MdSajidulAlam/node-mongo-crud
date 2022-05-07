780f1df9f7ef7d9a0bbe1b118262af10dc87f059
MdSajidulAlam <mdsajidulalam1245@gmail.com>
2022-05-07 20:03:10 +0600

    CRUD

---
+++ __steps.js
@@ -0,0 +1,49 @@
+/* 
+1. Create a node server with 5 steps
+1.1 create folder
+1.2 npm init
+1.3 npm i express cors mongodb
+1.4 script-dev: nodemon index.js
+1.5 create index.js
+1.6 user 5 steps to create a node server
+
+---------------------
+* Create atlas account
+---------------------
+1. sign up google access
+2. create cluster
+3. Create user dbuser and password
+4. Network Access --> ip address: allow all
+5. database --> Connect --> code copy paste in index.js
+
+
+---------------
+*CRUD operation
+---------------
+1. node mongodb CRUD >Fundamentals
+2. create async run function
+
+-------------------------------------------------
+*Integrate sending data from client to server
+-------------------------------------------------
+1. client side : Create form
+2. on submit get form data and create user object
+3. on server : Create user POST method to recive data on the backend
+4. on client side : set fetch with POST, headers,body
+5. make sure you return a json from the POST API
+
+-------------------------------
+* Load Data to client side
+-------------------------------
+1. create get API on the server
+2. Create a query object
+3. collection.find
+4. cursor.toArray()
+5. return the result
+6. from client useEffect and display like you have done before
+
+----------------------
+Delete
+----------------------
+1.
+*/
